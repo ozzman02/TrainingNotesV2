48e8ec2fd436d5429845a818f1b4514b902d56c5
Oscar Santamaría Venegas <oscar.santamaria@avantica.net>
2019-10-13 14:23:41 -0600

    update notes

--- Udemy/Spring4 - JPA - Hibernate/springmvc-java-config/src/main/java/springmvc/java/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package springmvc.java.service;
-
-public interface EmailService {
-
-	void sendEmail();
-}
--- Udemy/Spring4 - JPA - Hibernate/springmvc-java-config/src/main/java/springmvc/java/service/ServiceManager.java
+++
@@ -1,9 +0,0 @@
-package springmvc.java.service;
-
-import springmvc.java.domain.BlogPost;
-
-public interface ServiceManager {
-	
-	void sendBlogPost(BlogPost blogPost);
-
-}
