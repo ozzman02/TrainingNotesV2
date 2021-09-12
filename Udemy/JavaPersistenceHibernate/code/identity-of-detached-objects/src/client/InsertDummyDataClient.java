62e0147e5c795ec2d4fae14ec88dd3579cbe591a
Oscar Santamaria <osantamaria@gmail.com>
2021-09-12 01:03:55 -0600

    adding more code examples to JavaPersistence

---
+++ Udemy/JavaPersistenceHibernate/code/identity-of-detached-objects/src/client/InsertDummyDataClient.java
@@ -0,0 +1,31 @@
+package client;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import entity.Student;
+
+public class InsertDummyDataClient {
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("myPersistenceUnit");	
+		
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		Student student1 = new Student("2003CH50897", "Kevin Smith");
+		Student student2 = new Student("2008EE10999", "Sherry Morgan");
+		Student student3 = new Student("2009DS10954", "Rohit Sharma");
+		Student student4 = new Student("1987CS10007", "Sara Shield");
+		
+		em.persist(student1);
+		em.persist(student2);
+		em.persist(student3);
+		em.persist(student4);
+		 
+		em.getTransaction().commit();
+		em.close(); 
+		
+	}
+}
