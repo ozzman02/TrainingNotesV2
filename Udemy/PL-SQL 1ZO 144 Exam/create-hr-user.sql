ef27518fd2fe4b08b59552a946ff790a3d1775cd
Oscar Santamaria <osantamaria@gmail.com>
2022-07-02 15:14:33 -0600

    changing names

--- Udemy/PL-SQL 1ZO 144 Exam/create-hr-user.sql
+++ Udemy/PL-SQL 1ZO 144 Exam/create-hr-user.sql
@@ -3,6 +3,7 @@ alter session set container=orclpdb --only for 12c
 create user hr2 identified by hr2;
 grant create session to hr2;
 grant create table,create view to hr2; 
-grant  create procedure to hr2;
+grant create procedure to hr2;
+grant create sequence to hr2;
 grant create synonym, create trigger to hr2;
 grant unlimited tablespace to hr2;
