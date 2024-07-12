941bc8bd759b4e0a6a006f1f53654844b37acbf5
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-07-12 16:55:36 -0600

    updating notes on graphql

---
+++ Udemy/GraphQL-SpringBoot/dump-db_graphql_course-202407112131.sql
@@ -0,0 +1,111 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_graphql_course
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `address` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `street` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+INSERT INTO `address` VALUES (1,'Fengle','98721 Annamark Trail'),(2,'Mogi Guaçu','92632 Delaware Pass'),(3,'Xinxing','889 Bunker Hill Court'),(4,'Rydułtowy','69 Porter Park'),(5,'Kilkenny','52710 Toban Center'),(6,'Skënderbegas','234 Barnett Place'),(7,'Sanyang','2348 Walton Way'),(8,'Kota Kinabalu','20109 Eastwood Junction'),(9,'Salaberry-de-Valleyfield','5 Veith Circle'),(10,'Shuanggang','802 Hovde Terrace'),(23,'Mumbai','any street');
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKcaf6ht0hfw93lwc13ny0sdmvo` (`address_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'fbalsdon0@elpais.com','Filippo','Balsdon',1),(2,'bmccaughren1@hao123.com','Bronson','McCaughren',2),(3,'cnorton2@cbc.ca','Carr','Norton',3),(4,'wkittel3@newyorker.com	','Wadsworth','Kittel',4),(5,'nbonsey4@yahoo.com	','Natala','Bonsey',5),(6,'thurdman5@mediafire.com	','Tally','Hurdman',6),(7,'kplumer6@wsj.com','Karry','Plumer',7),(8,'bpohlak7@deviantart.com','Brandie','Pohlak',8),(9,'sdolley8@arizona.edu','Stacey','Dolley',9),(10,'idobby9@freewebs.com','Ingeberg','Dobby',10),(20,'sachink@gmail.com','Sachin','Kumar',23);
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subject`
+--
+
+DROP TABLE IF EXISTS `subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subject` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `marks_obtained` double DEFAULT NULL,
+  `subject_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `student_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK966amhbdgsgjsy3uqfhfim5mt` (`student_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subject`
+--
+
+LOCK TABLES `subject` WRITE;
+/*!40000 ALTER TABLE `subject` DISABLE KEYS */;
+INSERT INTO `subject` VALUES (1,80,'Java',1),(2,80,'React',2),(3,80,'Angular',3),(4,80,'Spring',4),(5,80,'Hibernate',5),(6,80,'Maven',6),(7,80,'AWS',7),(8,80,'Kubernetes',8),(9,80,'Git',9),(10,80,'Bash',10),(14,60,'Java',20),(15,65,'React',20);
+/*!40000 ALTER TABLE `subject` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'db_graphql_course'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-11 21:31:46
