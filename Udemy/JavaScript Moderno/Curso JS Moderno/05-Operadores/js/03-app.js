b49b6f304c0d68dae37a0674377a0b31291284c4
osantamaria <osantamaria@gmail.com>
2024-10-29 11:38:34 -0600

    operadores

---
+++ Udemy/JavaScript Moderno/Curso JS Moderno/05-Operadores/js/03-app.js
@@ -0,0 +1,10 @@
+let numero;
+console.log(numero);                // undefined
+console.log(typeof numero);         // undefined  
+
+let otroNumero = null;
+console.log(otroNumero);            // null
+console.log(typeof otroNumero);     // object
+
+console.log(numero == otroNumero);  // true
+console.log(numero === otroNumero); // fasle
