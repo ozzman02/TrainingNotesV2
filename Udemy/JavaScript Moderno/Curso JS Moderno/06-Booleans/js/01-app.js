b25e3fe11efdf6b97e2d6cdb4189bb9e72468f33
osantamaria <osantamaria@gmail.com>
2024-10-29 14:09:23 -0600

    booleanos

---
+++ Udemy/JavaScript Moderno/Curso JS Moderno/06-Booleans/js/01-app.js
@@ -0,0 +1,13 @@
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "true";
+
+console.log(typeof boolean1);
+console.log(typeof boolean2);
+
+console.log(boolean1 == boolean3);      // false
+console.log(boolean1 === boolean3);     // false
+
+const boolean4 = new Boolean(true);
+console.log(boolean4);
+console.log(typeof boolean4);
---
+++ Udemy/JavaScript Moderno/Curso JS Moderno/06-Booleans/js/03-app.js
@@ -0,0 +1,9 @@
+const autenticado = true;
+
+if (autenticado) {
+    console.log("Si puedes ver Netflix"); 
+} else {
+    console.log("No puedes verlo");
+}
+
+console.log(autenticado ? "Si esta autenticado" : "No esta autenticado");
