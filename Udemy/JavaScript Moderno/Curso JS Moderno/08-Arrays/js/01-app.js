91dddb14be7284d51e2795ba4a8fcc914e0576dc
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-10-29 22:05:29 -0600

    arreglos I

---
+++ Udemy/JavaScript Moderno/Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,8 @@
+const numeros = [10, 20, 30];
+console.log(numeros);
+
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+console.log(meses);
+
+const deTodo = ["Hola", 10, true, "si", null, {nombre: "Juan", trabajo: "Programador"}, [1,2,3]];
+console.log(deTodo);
