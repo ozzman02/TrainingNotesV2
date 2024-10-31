d06a9a0fd2628b9eed3a276685af422576decb18
osantamaria <osantamaria@gmail.com>
2024-10-31 16:29:12 -0600

    funciones I

---
+++ Udemy/JavaScript Moderno/Curso JS Moderno/09-Funciones/js/01-app.js
@@ -0,0 +1,13 @@
+// declaracion
+function sumar() {
+    console.log(2+2);
+}
+
+sumar();
+
+// expresion
+const sumar2 = function() {
+    console.log(3+3);
+}
+
+sumar2();
---
+++ Udemy/JavaScript Moderno/Curso JS Moderno/09-Funciones/js/02-app.js
@@ -0,0 +1,12 @@
+
+sumarV2();
+function sumarV2() {
+    console.log(2+2);
+}
+
+
+sumar2V2();
+const sumar2V2 = function() {
+    console.log(3+3);
+}
+
