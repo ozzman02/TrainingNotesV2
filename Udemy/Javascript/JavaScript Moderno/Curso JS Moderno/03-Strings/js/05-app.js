45ce2ab3b4c5302bad0e2576ea8c0825c910bea4
osantamaria <osantamaria@gmail.com>
2025-02-18 15:34:26 -0600

    js updates

--- Udemy/Javascript/JavaScript Moderno/Curso JS Moderno/03-Strings/js/05-app.js
+++ Udemy/Javascript/JavaScript Moderno/Curso JS Moderno/03-Strings/js/05-app.js
@@ -4,9 +4,9 @@ console.log(anotherProduct.replace('Pulgadas', '"'));
 console.log(anotherProduct.replace('Monitor', 'Monitor Curvo'));
 console.log(anotherProduct.slice(0, 10));
 console.log(anotherProduct.slice(8));
-console.log(anotherProduct.slice(2, 1));
+console.log(anotherProduct.slice(2, 1)); // no hace nada, no corta hacia atras
 console.log(anotherProduct.substring(0, 10));
-console.log(anotherProduct.substring(2, 1));
+console.log(anotherProduct.substring(2, 1)); // cambia el 1 por el 2
 
 const usuario = "Juan";
 console.log(usuario.substring(0, 1));
