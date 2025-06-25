b9a8a0e46e6714cb650140f63ccd0461c180dbc4
Oscar Santamaria Venegas <osantamaria@gmail.com>
2025-06-24 18:35:44 -0600

    refactoring notes

--- Udemy/React/NextJS/nextjs-complete-guide-course-resources-main/code/08-authentication/07-switch-auth-mode/lib/user.js
+++
@@ -1,8 +0,0 @@
-import db from './db';
-
-export function createUser(email, password) {
-  const result = db
-    .prepare('INSERT INTO users (email, password) VALUES (?, ?)')
-    .run(email, password);
-  return result.lastInsertRowid;
-}
--- Udemy/React/NextJS/nextjs-complete-guide-course-resources-main/code/08-authentication/10-logout/app/error.js
+++
@@ -1,5 +0,0 @@
-'use client';
-
-export default function RootError() {
-  return <h1>An error occurred!</h1>;
-}
